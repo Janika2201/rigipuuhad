e7c4acd3faf5c9891940bfdec750f464f1afbd1a
Opilane TTHK <opilane@tthk.ee>
2020-10-16 09:58:05 +0300

    Добавление анимации

--- rigipuuhad/Ulestousmispuha.xaml.cs
+++ rigipuuhad/Ulestousmispuha.xaml.cs
@@ -16,5 +16,14 @@ namespace rigipuuhad
         {
             InitializeComponent();
         }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await img.TranslateTo(0, 200, 2000, Easing.BounceIn);
+            await img.ScaleTo(2, 2000, Easing.CubicIn);
+            await img.RotateTo(360, 2000, Easing.SinInOut);
+            await img.ScaleTo(1,2000, Easing.CubicIn);
+            await img.TranslateTo(0, 0, 2000, Easing.BounceIn);
+        }
     }
 }
